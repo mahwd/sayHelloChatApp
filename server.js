0ca91f257bc0fcb69bd5384240f740e1e20794ce
Hmahwd <hasan.tagiyev@mdm.local>
2019-01-29 19:16:00 +0400

    converted to react webpack configured

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.set("view engine", "ejs");
 app.use(express.static("static"));
 app.use(express.static("node_modules"));
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/dist/index.html");
 });
 
 const server = require("http").createServer(app);
@@ -18,7 +18,6 @@ server.listen(3000, () => console.log("SERVER RUNNING!"));
 const io = require("socket.io")(server);
 
 io.sockets.on("connection", client => {
-  
   connections.push(client);
   console.log("Connected: %s sockets connected!", connections.length);
   client.on("disconnect", () => {
@@ -29,5 +28,4 @@ io.sockets.on("connection", client => {
   client.on("user_message", data => {
     io.emit("user_message", data);
   });
-
 });
